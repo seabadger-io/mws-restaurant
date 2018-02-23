468521171699e94553e22a932a81757e502d84cd
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-02-23 16:22:39 +0000

    refactor: Prepare restaurant list for responsive
    Redesign restaurant list to prepare for responsive design.
    Add necessary classes and replace fix sizing with flexible.

--- js/main.js
+++ js/main.js
@@ -137,30 +137,41 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'restaurant-container';
+
+  const imgdiv = document.createElement('div');
+  imgdiv.className = 'restaurant-img';
 
   const image = document.createElement('img');
-  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  imgdiv.append(image);
+
+  li.append(imgdiv);
+
+  const summarydiv = document.createElement('div');
+  summarydiv.className = 'restaurant-summary';
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  summarydiv.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  summarydiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  summarydiv.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.className = 'button';
+  summarydiv.append(more);
 
-  return li
+  li.append(summarydiv);
+
+  return li;
 }
 
 /**
