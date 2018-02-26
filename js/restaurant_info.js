082db3fbfef5e05bedece93a380e8e0780e19c03
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-02-26 13:46:30 +0000

    refactor: Prepare restaurant details for responsive design
    Prepeare restaurant details for redesign to use responsive design.
    Remove styling and reorganize page layout

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -56,7 +56,6 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
-  image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
