2c326ea737e639da7715a0c10ae8eec5d6ecf358
Gabor Szoboszlai <itgipsy@itgipsy.com>
2018-03-16 14:09:10 +0000

    feat: Defer CSS
    Defer loading of CSS in restaurant list and details

---
+++ penthouse.js
@@ -0,0 +1,18 @@
+const penthouse = require('penthouse');
+const fs = require('fs');
+
+penthouse({
+  url: 'http://localhost:8000',
+  css: './css/styles.css'
+})
+.then((criticalCss) => {
+  fs.writeFileSync('./css/critical-main.css', criticalCss);
+});
+
+penthouse({
+    url: 'http://localhost:8000/restaurant.html?id=1',
+    css: './css/styles.css'
+  })
+  .then((criticalCss) => {
+    fs.writeFileSync('./css/critical-detail.css', criticalCss);
+  });
